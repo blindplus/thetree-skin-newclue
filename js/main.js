698a36560403c774bdb754f0b7d30cd7b738b836
blindplus <noahjeon12@gmail.com>
2026-02-13 18:49:34 +1100

    edited wow

--- js/main.js
+++ js/main.js
@@ -26,36 +26,24 @@ function selectByTheme(light, dark) {
     return localConfig['wiki.theme'] === 'dark' ? dark : light;
 }
 
-const brand_color = () => selectByTheme(CONFIG['theme_color'] ?? '#4188f1', '#2d2f34');
+const brand_color = () => selectByTheme(CONFIG['skin.newclue.brand_color_1'] ?? '#4188f1', '#2d2f34');
 
 const style = document.createElement('style');
 function setStyles() {
     const styles = {
         '--clue-brand-color': brand_color(),
-        '--clue-brand-dark-color': selectByTheme(CONFIG['skin.clue.brand_dark_color_1'] ?? darkenColor(brand_color()), '#16171a'),
-        '--clue-brand-bright-color': selectByTheme(CONFIG['skin.clue.brand_bright_color_1'] ?? lightenColor(brand_color()), '#383b40'),
-        '--clue-brand-notice-color': selectByTheme(CONFIG['skin.clue.brand_notice_color'] ?? '#d9edf7', '#d9edf7'),
-        '--clue-navbar-logo-image': CONFIG['logo_image'] && `url(${CONFIG['logo_image']})`,
-        '--clue-navbar-logo-minimum-width': CONFIG['skin.clue.navbar_logo_minimum_width'],
-        '--clue-navbar-logo-width': CONFIG['skin.clue.navbar_logo_width'],
-        '--clue-navbar-logo-size': CONFIG['skin.clue.navbar_logo_size'],
-        '--clue-navbar-logo-padding': CONFIG['skin.clue.navbar_logo_padding'],
-        '--clue-navbar-logo-text-padding': CONFIG['skin.clue.navbar_logo_text_padding'],
-        '--clue-navbar-logo-minimum-margin-left': CONFIG['skin.clue.navbar_logo_minimum_margin_left'],
-        '--clue-navbar-logo-margins-left': CONFIG['skin.clue.navbar_logo_margin_left'],
-        '--clue-navbar-logo-minimum-margin': CONFIG['skin.clue.navbar_logo_minimum_margin'],
-        '--clue-navbar-logo-minimum-margin-top': CONFIG['skin.clue.navbar_logo_minimum_margin_top'],
-        '--clue-navbar-logo-margin-top': CONFIG['skin.clue.navbar_logo_margin_top'],
-        '--clue-navbar-logo-margin': CONFIG['skin.clue.navbar_logo_margin'],
-        '--clue-main-header-background-color': CONFIG['skin.clue.main_header_background_color'],
-        '--clue-main-content-link-color': CONFIG['skin.clue.main_content_link_color'],
-        '--clue-main-content-link-dark-color': CONFIG['skin.clue.main_content_link_dark_color'],
-        '--clue-main-content-username-color': CONFIG['skin.clue.main_content_username_color'],
-        '--clue-main-content-username-dark-color': CONFIG['skin.clue.main_content_username_dark_color'],
+        '--clue-brand-dark-color': selectByTheme(CONFIG['skin.newclue.brand_dark_color_1'] ?? darkenColor(brand_color()), '#16171a'),
+        '--clue-brand-bright-color': selectByTheme(CONFIG['skin.newclue.brand_bright_color_1'] ?? lightenColor(brand_color()), '#383b40'),
+        '--clue-navbar-logo-image': CONFIG['skin.newclue.navbar_logo_image'] || (CONFIG['wiki.logo_url'] && `url(${CONFIG['wiki.logo_url']})`),
+        '--clue-navbar-logo-minimum-width': CONFIG['skin.newclue.navbar_logo_minimum_width'],
+        '--clue-navbar-logo-width': CONFIG['skin.newclue.navbar_logo_width'],
+        '--clue-navbar-logo-size': CONFIG['skin.newclue.navbar_logo_size'],
+        '--clue-navbar-logo-padding': CONFIG['skin.newclue.navbar_logo_padding'],
+        '--clue-navbar-logo-margin': CONFIG['skin.newclue.navbar_logo_margin'],
         '--brand-color-1': 'var(--clue-brand-color)',
-        '--brand-color-2': selectByTheme(CONFIG['skin.clue.brand_color_2'] ?? 'var(--clue-brand-color)', 'var(--clue-brand-color)'),
+        '--brand-color-2': selectByTheme(CONFIG['skin.newclue.brand_color_2'] ?? 'var(--clue-brand-color)', 'var(--clue-brand-color)'),
         '--brand-bright-color-1': 'var(--clue-brand-bright-color)',
-        '--brand-bright-color-2': selectByTheme(CONFIG['skin.clue.brand_bright_color_2'] ?? 'var(--clue-brand-bright-color)', 'var(--clue-brand-bright-color)'),
+        '--brand-bright-color-2': selectByTheme(CONFIG['skin.newclue.brand_bright_color_2'] ?? 'var(--clue-brand-bright-color)', 'var(--clue-brand-bright-color)'),
         '--text-color': selectByTheme('#373a3c', '#ddd'),
         '--article-background-color': selectByTheme('#fff', '#1d2023')
     }
